a36d71318d9098a92bc8db62f23e3c74e568067c
Kittyfisto <kittyfisto87@gmail.com>
2018-05-05 19:01:21 +0200

    Removed pointless IsRequired from testdata

--- IsabelDb.Test/Entities/V1/Comic.cs
+++ IsabelDb.Test/Entities/V1/Comic.cs
@@ -5,10 +5,10 @@ namespace IsabelDb.Test.Entities.V1
 	[DataContract(Name = "Comic", Namespace = "IsabelDb.Test.Entities")]
 	public sealed class Comic
 	{
-		[DataMember(IsRequired = true)]
+		[DataMember]
 		public string Name { get; set; }
 
-		[DataMember(IsRequired = true)]
+		[DataMember]
 		public string Writer { get; set; }
 	}
 }
--- IsabelDb.Test/Entities/V2/Comic.cs
+++ IsabelDb.Test/Entities/V2/Comic.cs
@@ -5,13 +5,13 @@ namespace IsabelDb.Test.Entities.V2
 	[DataContract(Name = "Comic", Namespace = "IsabelDb.Test.Entities")]
 	public sealed class Comic
 	{
-		[DataMember(IsRequired = true)]
+		[DataMember]
 		public string Name { get; set; }
 
-		[DataMember(IsRequired = true)]
+		[DataMember]
 		public string Writer { get; set; }
 
-		[DataMember(IsRequired = false)]
+		[DataMember]
 		public string Artist { get; set; }
 	}
 }
