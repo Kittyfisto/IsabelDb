d9325ac698368c43d0c2dac556c44f3a8518afe1
Simon Mießler <simon.miessler@gmx.net>
2021-11-13 14:00:19 +0100

    Fixing build...

--- src/IsabelDb.Browser.Test/Properties/AssemblyInfo.cs
+++ src/IsabelDb.Browser.Test/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("IsabelDb.Browser.Test")]
-[assembly: AssemblyCopyright("Copyright © Simon Mießler 2018")]
+[assembly: AssemblyCopyright("Copyright © Simon Mießler 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
